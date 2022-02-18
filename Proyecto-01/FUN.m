d0dbdb49635131da030d237ae3b0afb84a718744
Alejandro Castellanos <a.castellanoss@uniandes.edu.co>
2022-02-17 19:09:27 -0500

    parameters specification

--- Proyecto-01/FUN.m
+++ Proyecto-01/FUN.m
@@ -1,14 +1,12 @@
-function px=FUN(t,x);
-global K1 K2 K3 K4 K5 K6 K7 K8  
-px=zeros(6,1);
-px(1)=K1-K2*x(1)*x(5);
-px(2)=K2*x(1)*x(5)-K3*x(2)+K4*x(3);
-px(3)=K3*x(2)-K4*x(3)-K5*x(3)*x(5)+K6*x(4);
-px(4)=K5*x(3)*x(5)-K6*x(4)-K7*x(4);
-px(5)=-K2*x(1)-K5*x(3)+K8*x(6);
-px(6)=K2*x(1)+K5*x(3)-K8*x(6);
-
-
+function dc=Threonine(t,x);
+global K1 K2 K3 K4 K5 K6 K7 K8 K9 K10 K11 K12 K13 K14 K15 K16 K17 K18 K19 K20 K21 K22 K23 K24 K25 K26 K27 K28 K29 K30 K31 K32 K33 K34 K35 K36  
+dc=zeros(6,1);
+dc(1)=;
+dc(2)=;
+dc(3)=;
+dc(4)=;
+dc(5)=;
+dc(6)=;
 
 end
 
